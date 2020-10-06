7df417ab1b4dc15f777a2906a6c803ce6323e7c0
marimelon <29140293+marimelon@users.noreply.github.com>
2020-10-06 15:40:40 +0900

    初期化

---
+++ src/main/java/oit/is/igakiigaiga/hondahonhon/HondahonhonApplication.java
@@ -0,0 +1,13 @@
+package oit.is.igakiigaiga.hondahonhon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HondahonhonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HondahonhonApplication.class, args);
+	}
+
+}
