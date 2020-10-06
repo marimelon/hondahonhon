7dc804bac293d838f3136591062cf196ea78a458
e1b18049 <e1b18049@oit.ac.jp>
2020-10-06 16:46:33 +0900

    sample2-1追加

---
+++ src/main/java/oit/is/igakiigaiga/hondahonhon/controller/Sample21Controller.java
@@ -0,0 +1,22 @@
+package oit.is.igakiigaiga.hondahonhon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Sample21Controller {
+
+  /**
+   * sample21というGETリクエストがあったら，sample21()を呼び出して，sample21.htmlを返すメソッド
+   *
+   * @return
+   */
+  @GetMapping("/sample21")
+  public String sample21() {
+    return "sample21.html";
+  }
+}
